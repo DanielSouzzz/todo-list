88059a1420c0b7313784081d77d1da9d737d57e6
Danielsouzzz <danielsouzanovo@gmail.com>
2025-11-13 22:37:37 -0300

    feat: annotate Repository class and create Service class

---
+++ src/main/java/com/danielsouza/todolist/service/TaskService.java
@@ -0,0 +1,42 @@
+package com.danielsouza.todolist.service;
+
+import com.danielsouza.todolist.model.Task;
+import com.danielsouza.todolist.repository.TaskRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public  Task save(Task task) {
+        return this.taskRepository.save(task);
+    }
+
+    public List<Task> listTasks() {
+        return this.taskRepository.findAll();
+    }
+
+    public Task getTaskById(Long id) {
+        return this.taskRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(String.format("Task with id %s not found", id)));
+    }
+
+    public Task updateTask(Task task) {
+        Task oldTask = this.getTaskById(task.getId());
+
+        oldTask.setTitle(task.getTitle());
+        oldTask.setCompleted(task.isCompleted());
+
+        return this.taskRepository.save(oldTask);
+    }
+
+    public void deleteTaskById(Long id) {
+        this.taskRepository.deleteById(id);
+    }
+}
